95a721f62ff60c9adb7401d5adc0b74603e2b945
polderdude <paul.polderman@gmail.com>
2011-10-08 13:03:31 +0200

    Test2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(121, 108);
+			this.button1.Location = new System.Drawing.Point(37, 51);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
